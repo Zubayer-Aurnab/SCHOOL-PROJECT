e354beaa0d83163eee1c32bce27174415ef71767
Aurnab <zubayeraurnab@gmail.com>
2024-04-05 16:36:38 +0600

    notice board updated

--- src/Components/ui/Notice/Notice.jsx
+++ src/Components/ui/Notice/Notice.jsx
@@ -1,12 +1,19 @@
 // import mic from '../../../assets/Mic.jpg'
 import niyog from '../../../assets/niyog.png'
+import newspaper from '../../../assets/newspaper.png'
+import pdf from '../../../assets/নিয়োগ বিজ্ঞপ্তি-2024.pdf'
 const Notice = () => {
     return (
         <div className="bg-[#F7F6E7]">
             <div className="w-4/5 mx-auto ">
                 <h1 className="text-3xl font-bold  text-gray-800 text-center my-8 underline">NOTICE </h1>
-                <div className='flex justify-center mb-10'>
-                    <img src={niyog} alt="" className='' />
+                <div className='flex flex-col items-center justify-center mb-10 '>
+                    <img src={niyog} alt="" className='w-1/2' />
+                    <button className='bg-[#314E52] text-[#F7F6E7] w-1/2 p-2 rounded-xl mt-2 font-bold'><a href={pdf} download='niyog biggopti'>Download</a></button>
+                </div>
+                <div className='flex flex-col items-center justify-center mb-10 '>
+                    <img src={newspaper} alt="" className='w-1/2' />
+                    <button className='bg-[#314E52] text-[#F7F6E7] w-1/2 p-2 rounded-xl mt-2 font-bold'><a href={pdf} download='niyog biggopti'>Download</a></button>
                 </div>
             </div>
         </div>
--- src/Components/ui/NoticeBoard/NoticeBoarrd.jsx
+++ src/Components/ui/NoticeBoard/NoticeBoarrd.jsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import niyog from '../../../assets/niyog.png'
+import newspaper from '../../../assets/newspaper.png'
+
 
 const NoticeBoard = () => {
     return (
         <div className="border-4 border-[#314E52] rounded-xl">
             <h1 className="text-2xl font-semibold  text-center bg-[#314E52] text-[#F7F6E7] pb-2"> Notice Board</h1>
-            <Link to={'/notice'}>
-                <div className="mt-8 p-2 flex gap-3 bg-[#E7E6E1] m-2">
-                    <img src={niyog} alt="" className="h-28" />
-                    <p className='text-sm text-blue-600'>
-                        শিক্ষক/শিক্ষিকা/কর্মচারী নিয়োগ  দেওয়া হইবে.... ।
-                    </p>
-                </div>
-            </Link>
+            <div className="">
+                <Link to={'/notice'}>
+                    <div className=" p-2 flex gap-3 bg-[#E7E6E1] m-2">
+                        <img src={niyog} alt="" className="h-28" />
+                        <p className='text-sm text-blue-600'>
+                            শিক্ষক/শিক্ষিকা/কর্মচারী নিয়োগ  দেওয়া হইবে.... ।
+                        </p>
+                    </div>
+                </Link>
+                <Link to={'/notice'}>
+                    <div className=" mt-6 p-2 flex gap-3 bg-[#E7E6E1] m-2">
+                        <img src={newspaper} alt="" className="h-28" />
+                        <p className='text-sm text-blue-600'>
+                            শিক্ষক/শিক্ষিকা/কর্মচারী নিয়োগ  দেওয়া হইবে.... ।
+                        </p>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 };
