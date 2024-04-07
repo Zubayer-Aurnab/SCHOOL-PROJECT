ed22669349db0acf0483f1c87d51c972cdce8a6a
Aurnab <zubayeraurnab@gmail.com>
2024-04-07 22:13:50 +0600

    Nav bar updated

--- src/Components/shared/Nav/NavBar.jsx
+++ src/Components/shared/Nav/NavBar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../../assets/logo2.png"
+import { FaLocationDot } from "react-icons/fa6";
 
 const NavBar = () => {
     return (
         <div>
             {/* UPPER NAV */}
             <div className="h-42 bg-[#314E52]  ">
-                <div className="flex justify-center gap-10 py-4 px-2">
+                <div className="flex justify-center items-center gap-8 lg:gap-20 py-4 px-2">
                     <div>
                         <img className="h-12 w-12   lg:h-28 lg:w-28 rounded-full " src={logo} alt="" />
                     </div>
@@ -16,7 +17,11 @@ const NavBar = () => {
                         }}
                         className="text-center ">
                         <h1 className="text-[#F7F6E7] text-[12px] lg:text-3xl font-semibold py-2 ">Khord Ailchara Secondary School</h1>
-                        <h1 className="text-[#F7F6E7] text-[10px] lg:text-2xl font-semibold">খোর্দ আইলচারা মাধ্যমিক বিদ্যালয়</h1>
+                        <h1 className="text-[#F7F6E7] text-[10px] lg:text-2xl font-semibold pb-2">খোর্দ আইলচারা মাধ্যমিক বিদ্যালয়</h1>
+                        <div className="flex justify-center gap-4">
+                            <h1 className="text-[#F7F6E7] text-[8px] lg:text-base ">ইআইআইএন: ১১৭৭৬৬   </h1>
+                            <h1 className="text-[#F7F6E7] text-[8px] lg:text-base flex justify-center items-center gap-2"><FaLocationDot />কুষ্টিয়া  </h1>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -24,7 +29,7 @@ const NavBar = () => {
 
 
             {/* bottom nav */}
-            <div className="bg-[#F2A154]  h-12 flex gap-10 justify-center py-2">
+            <div className="bg-[#F2A154]  h-12 flex gap-10 justify-center py-2  overflow-x-auto">
                 <NavLink
                     to="/"
                     className={({ isActive, isPending }) =>
@@ -48,6 +53,38 @@ const NavBar = () => {
                     }
                 >
                     Notice
+                </NavLink>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52]  font-semibold" : "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52] font-semibold"
+                    }
+                >
+                    Admission
+                </NavLink>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52]  font-semibold" : "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52] font-semibold"
+                    }
+                >
+                    Academy
+                </NavLink>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52] font-semibold" : "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52] font-semibold"
+                    }
+                >
+                    Administration
+                </NavLink>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52] font-semibold" : "text-[#314E52] lg:text-xl hover:border-b-4 border-[#314E52] font-semibold"
+                    }
+                >
+                    Gallery
                 </NavLink>
 
             </div>
--- src/Components/ui/Notice/Notice.jsx
+++ src/Components/ui/Notice/Notice.jsx
@@ -1,4 +1,3 @@
-// import mic from '../../../assets/Mic.jpg'
 import niyog from '../../../assets/niyog.png'
 import newspaper from '../../../assets/newspaper.png'
 import pdf from '../../../assets/re.pdf'
@@ -12,7 +11,7 @@ const Notice = () => {
                     <h1 className='text-center text-[15px] lg:text-xl text-blue-600 font-bold mb-4 lg:mb-8'>শিক্ষক/শিক্ষিকা/কর্মচারী নিয়োগ  দেওয়া হইবে.... ।২৬/০৩/২০২৪  হইতে  ০৯/০৪/২০০২৪</h1>
                     <div className='flex flex-col items-center justify-center '>
                         <img src={niyog} alt="" className='w-1/2 ' />
-                        <button className='bg-[#314E52] text-[#F7F6E7] w-1/2 p-1 lg:p-2 rounded-xl mt-2 font-bold'><a href={niyog} download='niyog biggopti'>Download</a></button>
+                        <a href={pdf} download='niyog biggopti' className='bg-[#314E52] text-[#F7F6E7] w-1/2 p-1 lg:p-2 rounded-xl mt-2 font-bold text-center'>    <button >Download</button></a>
                     </div>
                 </div>
 
@@ -21,7 +20,7 @@ const Notice = () => {
                     <h1 className='text-center text-[15px] lg:text-xl text-blue-600 font-bold mb-4 lg:mb-8'>শিক্ষক/শিক্ষিকা/কর্মচারী নিয়োগ  দেওয়া হইবে.... ।২৬/০৩/২০২৪  হইতে  ০৯/০৪/২০০২৪</h1>
                     <div className='flex flex-col items-center justify-center mb-10 '>
                         <img src={newspaper} alt="" className='w-1/2' />
-                        <button className='bg-[#314E52] text-[#F7F6E7] w-1/2 p-1 lg:p-2 rounded-xl mt-2 font-bold'><a href={newspaper} download='niyog biggopti'>Download</a></button>
+                        <a href={newspaper} download='niyog biggopti' className='bg-[#314E52] text-[#F7F6E7] w-1/2 p-1 lg:p-2 rounded-xl mt-2 font-bold text-center' >    <button >Download</button></a>
                     </div>
                 </div>
             </div>
